e867c992aad04a5c930813d80f5847856f839b75
Mintboi <martinpswift@gmail.com>
2024-11-26 13:54:25 +0000

    Initial commit

---
+++ assignment-ChatGPT/src/main/java/com/example/assignment_ChatGPT/AssignmentChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_ChatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentChatGptApplication.class, args);
+	}
+
+}
---
+++ assignment-ChatGPT/src/test/java/com/example/assignment_ChatGPT/AssignmentChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_ChatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
